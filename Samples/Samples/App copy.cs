631982582a7c5d8addc75b09c1362cef05f2a6f3
Allan Ritchie <aritchie@users.noreply.github.com>
2016-01-14 10:35:27 -0500

    amend commit 1

---
+++ Samples/Samples/App copy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Samples.Views;
+using Xamarin.Forms;
+
+
+namespace Samples {
+
+	public class App : Xamarin.Forms.Application {
+
+		public App() {
+			this.MainPage = new NavigationPage(new HomeView());
+		}
+    }
+}
